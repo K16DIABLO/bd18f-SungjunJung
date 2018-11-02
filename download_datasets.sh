f606ac59a647776bd0202964093175d1d19ee914
K16DIABLO <miguel92@naver.com>
2018-11-02 14:42:54 +0900

    Dataset download

---
+++ download_datasets.sh
@@ -0,0 +1,7 @@
+wget https://download.open.fda.gov/food/enforcement/food-enforcement-0001-of-0001.json.zip
+unzip food-enforcement-0001-of-0001.json.zip
+mv food-enforcement-0001-of-0001.json input1.txt
+
+wget https://download.open.fda.gov/food/event/food-event-0001-of-0001.json.zip
+unzip food-event-0001-of-0001.json.zip
+mv food-event-0001-of-0001.json input2.txt
